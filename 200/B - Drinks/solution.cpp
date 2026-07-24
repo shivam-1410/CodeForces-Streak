5041cadd20400520ba76ce393a4a519f682dba68
shivam-1410 <shivamrpatel.14105@gmail.com>
2026-07-25 00:33:38 +0530

    Add Drinks [B] from Codeforces

---
+++ 200/B - Drinks/solution.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <iomanip>
+using namespace std;
+ 
+int main() {
+    int n;
+    cin >> n;
+ 
+    double sum = 0, x;
+ 
+    for (int i = 0; i < n; i++) {
+        cin >> x;
+        sum += x;
+    }
+ 
+    cout << fixed << setprecision(12) << sum / n;
+ 
+    return 0;
+}
