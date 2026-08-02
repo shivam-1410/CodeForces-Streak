c7dfa21af725159c04809f492846aac44adabd7c
shivam-1410 <shivamrpatel.14105@gmail.com>
2026-08-02 23:18:44 +0530

    Add Queue at the School [B] from Codeforces

---
+++ 266/B - Queue at the School/solution.java
@@ -0,0 +1,24 @@
+import java.util.*;
+ 
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+ 
+        int n = sc.nextInt();
+        int t = sc.nextInt();
+ 
+        char[] queue = sc.next().toCharArray();
+ 
+        while (t-- > 0) {
+            for (int i = 0; i < n - 1; i++) {
+                if (queue[i] == 'B' && queue[i + 1] == 'G') {
+                    queue[i] = 'G';
+                    queue[i + 1] = 'B';
+                    i++;
+                }
+            }
+        }
+ 
+        System.out.println(new String(queue));
+    }
+}
