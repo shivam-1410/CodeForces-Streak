300c5c2726cd7145f71a7e759c9a93cde3cebdcb
shivam-1410 <shivamrpatel.14105@gmail.com>
2026-08-11 22:33:46 +0530

    Add Young Physicist [A] from Codeforces

---
+++ 69/A - Young Physicist/solution.java
@@ -0,0 +1,31 @@
+import java.util.*;
+ 
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+ 
+        int n = sc.nextInt();
+ 
+        int sumX = 0;
+        int sumY = 0;
+        int sumZ = 0;
+ 
+        for (int i = 0; i < n; i++) {
+            int x = sc.nextInt();
+            int y = sc.nextInt();
+            int z = sc.nextInt();
+ 
+            sumX += x;
+            sumY += y;
+            sumZ += z;
+        }
+ 
+        if (sumX == 0 && sumY == 0 && sumZ == 0) {
+            System.out.println("YES");
+        } else {
+            System.out.println("NO");
+        }
+ 
+        sc.close();
+    }
+}
